22a56ea62fdf1c1648d5d4aea172c8692b2d000e
NakOMAX <nachogarceran@yahoo.com>
2020-03-12 12:36:35 +0100

    The Boost Graph implementation

--- src/Map.cpp
+++ src/Map.cpp
@@ -1,77 +1,66 @@
 #include "Map.h"
-#include "Event.h"
 
+#include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <vector>
-using namespace std;
 
-#define MAP_TXT 0
-#define MAP_SDL 1
-
-Map::Map(unsigned short int layers = 10)
+Map::Map(unsigned short int totalLayers)
 {
   // Initialization of variables
-  srand (time(NULL));
-  unsigned short int eventsInLayer = new unsigned short int [layers];
-  nodeTree = new vector<Node>;
-  edgeArray = new vector<Edge>;
-  unsigned short int nEvents = 1;
-  unsigned short int random;
-  unsigned short int i, j, k;
+  myMap = GraphMap();
+  eventsInLayer = new short int[totalLayers];
+  for (unsigned short int i = 0; i<totalLayers; i++)
+      eventsInLayer[i]=0;
 
-  // Boss layer set-up
-  eventsInLayer[0]=1;
-  Node currentEvent;
-  //currentEvent->event=new Boss;
-  currentEvent.content = 0; // test de contenu
-  currentEvent.posY = 0;
-  nodeTree->push_back(currentEvent);
+  // Boss layer
+  Vertex vertexF = add_vertex(myMap);
+  myMap[vertexF].event=BOSS_EVENT;
+  myMap[vertexF].layer=totalLayers-1;
+  eventsInLayer[totalLayers-1]++;
 
-  Edge currentEdge;
-
-  // For each layer we create a number of events that depends on the number of events in the upper layer
-  for (i = 1; i < layers; i++)
-  {
-    eventsInLayer[i]=0;
-
-    // Each event in the upper layer has 1 or 2 lower events, randomly
-    // j is the index of the current upper event
-    for (j = nEvents - eventsInLayer[i-1]; j < nEvents ; j++)
-    {
-      random=rand()%2 + 1;
-      eventsInLayer[i]+=random;
-      currentEdge.up = &nodeTree->at(j);
-
-      // Initialization of events
-      for (k = 0; k < random; k++)
-      {
-        currentEvent.posY = i;
-        //param
-        nodeTree->push_back(temp);
-
-        currentEdge.down = &nodeTree->back();
-        edgeArray->push_back(currentEdge);
-
-        currentEvent.connects.push_back(&edgeArray->back());
-      }
-
-    }
-    nEvents+=eventsInLayer[i];
-    for (k = eventsInLayer[i]; k > 0; k--) {
-      nodeTree->at(nEvents-k).posX = k;
-    }
-  }
+  // Start of the recursive method
+  createVerticesBelow(vertexF, totalLayers-2);
 }
 
 Map::~Map()
 {
-  size=nodeTree->size();
-  for (int i = 0; i < size; i++)
-  {
-    delete nodeTree->at(i);
-  }
-  delete nodeTree;
-  delete eventsInLayer;
+  delete [] eventsInLayer;
+  std::cout<<"Map succesfully destroyed"<<std::endl;
+}
+
+void Map::createVerticesBelow(Vertex vertexF, unsigned short int layer)
+{
+    Vertex vertexS;
+    unsigned short int randomVertices = rand()%2+1;
+    for (unsigned short int i = 0; i < randomVertices; i++)
+    {
+        vertexS = add_vertex(myMap);
+        myMap[vertexS].layer=layer;
+        add_edge(vertexS, vertexF, myMap);
+        if (layer!=0)
+            createVerticesBelow(vertexS, layer-1);
+    }
+}
+
+void Map::drawMap(short int mode)
+{
+    switch(mode)
+    {
+        default:
+            auto vPair = vertices(myMap);
+            int i = 0;
+            for (auto iter = vPair.first; iter!=vPair.second; iter++)
+            {
+                std::cout<< "Vertex n" << i++<< " is in layer "<< myMap[*iter].layer
+                <<std::endl;
+            }
+    }
+}
+
+int main(void)
+{
+    Map pokemonMap(5);
+    pokemonMap.drawMap();
+    return 0;
 }
--- src/Map.h
+++ src/Map.h
@@ -1,66 +1,74 @@
 #ifndef POKE_MAP_H
 #define POKE_MAP_H
 
-#include "Event.h"
-#include <vector>
-using namespace std;
+
+#include <boost/graph/adjacency_list.hpp>
+
+#define MAP_TXT 0
+#define MAP_SDL 1
+
+#define BOSS_EVENT 0
+#define WILD_EVENT 1
+#define TRAINER_EVENT 2
+#define HEAL_EVENT 3
 
 struct Node{
   //Event * event;
-  unsigned short int content;
+  unsigned short int event;
+  /** @brief 0<=layer<totalLayers */
+  unsigned short int layer;
+
+  //For SDL purposes
   unsigned short int posX;
   unsigned short int posY;
 
-  unsigned short int state; // visited, current, unreachable, reachable
-  vector<Edge *> connects;
+  // Visited, current, unreachable, reachable
+  unsigned short int state;
 };
 
-struct Edge {
-  Node * up;
-  Node * down;
-};
+typedef boost::adjacency_list<boost::listS, boost::listS,
+                              boost::undirectedS, Node>
+                              GraphMap;
+typedef boost::graph_traits<GraphMap>::vertex_descriptor Vertex;
 
 class Map{
 public:
   /** @brief Class constructor */
-  /** The class constructor randomly generates a map of the desired number of layers */
+  /** The class constructor randomly generates a map of the desired
+  number of layers */
   /** @param layers Number of layers that the map will have. Minimun: 1 */
-  Map(unsigned short int layers = 10);
+  Map(unsigned short int totalLayers = 10);
 
   /** @brief Class destructor. Deallocates memory. */
   ~Map();
 
   /** @brief paths and numberOfPaths updated to start the run */
-  /** paths become an array of size numberOfPaths that stocks the first events id, where the run will start */
+  /** paths become an array of size numberOfPaths that stocks the first
+  events id, where the run will start */
   /** @param paths array where the ids will be stocked */
   /** @param numberOfPaths size of paths array */
   void getStarts(Node *& starts, unsigned short int & numberOfPaths);
 
-  /** @brief find a path between two nodes*/
-  /** pathes become an array that contains every edge taken to reach the end*/
-  /** @param p1 starting node*/
-  /** @param p2 end point */
-  /** @param pathes edge array result */
-  void astar(const Node * p1, const Node * p2, Edge * & pathes);
-
   /** @brief Returns pointer to the event number eventId */
   /** @param eventId Current event Id */
-  Event * getEvent(const Node * n);
+  //Event * getEvent(const Node * n);
 
   /** @brief Draws the map*/
   /** Draws the map in console mode or in graphic mode with SDL */
-  /** @param mode MAP_TXT to console mode MAP_SDL to graphic mode */
-  void drawMap(unsigned short int mode);
+  /** @param mode MAP_TXT to txt mode MAP_SDL to graphic mode.
+  Default: console mode, single print in the terminal*/
+  void drawMap(short int mode=-1);
 
-protected:
-  /** @brief Array where the events are stocked */
-  vector<Node>* nodeTree;
+private:
+  // Container that stores the map structure
+  GraphMap myMap;
 
-  /** @brief Array where edges are stocked */
-  vector<Edge>* edgeArray;
-  
-  /** @brief Number of events in each layer*/
-  unsigned short int * eventsInLayer;
+  // Number of events in each layer
+  short int * eventsInLayer;
+
+  // Creates one or two vertices in layer linked to vertxF. Recursive method.
+  void createVerticesBelow(Vertex vertexF, unsigned short int layer);
+
 };
 
 #endif
