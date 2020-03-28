abb24f15a78bbc0f24eefc128ab0665a73a3297b
Marie-Liesse <marie-liesse.demont@etu.univ-lyon1.fr>
2020-03-28 19:17:41 +0100

    creation potion

--- src/item/Potion.cpp
+++ src/item/Potion.cpp
@@ -1,1 +1,21 @@
 
+#include "Pokemon.h"
+#include "Potion.h"
+
+#include <iostream>
+//#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+using namespace std;
+
+#define const unsigned short int HPMAX=100
+
+
+Potion :: Potion () {
+
+    id = 001;
+    amount =0;
+    name = "Potion";
+}
+
+Potion :: ~Potion () {}
