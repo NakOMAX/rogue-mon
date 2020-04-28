19f7dd5efcaac5bff9d020a751aa3b696459e5e3
UCAR IBRAHIM p1706473 <ibrahim.ucar@etu.univ-lyon1.fr>
2020-04-28 16:57:38 +0200

    Item compile + debut debug player !

--- src/item/Item.cpp
+++ src/item/Item.cpp
@@ -1,10 +1,10 @@
 #include "Item.h"
-#include "Pokemon.h"
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <vector>
+#include <string>
 using namespace std;
 
 Item :: Item(){}
@@ -18,33 +18,12 @@ Item :: Item(unsigned int newId, string newName, unsigned short int newAmount=0)
 
 Item :: ~Item(){}
 
-unsigned short int Pokemon :: getId ()const
+unsigned short int Item::getId ()
 {
     return id;
 }
 
-string Pokemon :: getName ()const
+string Item::getName()
 {
     return name;
-}
-
-/*Item :: useItem(){//cet intitulé est bizarre
-    amount--;
-}*/
-
-void Item :: action (Pokemon* Pok)
-{
-  amount--;
-  if( amount<=0)
-  {
-    
-  }
-}
-
-void Item :: operator= (Item copy)
-{
-  name=copy.name ;
-  amount = copy.amount;
-  id = copy.id;
-
 }
